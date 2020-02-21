650fa67b2875baea211f0aefb772a55d88503954
Jonathan Mucha <jonmucha@gmail.com>
2020-02-21 15:47:03 -0500

    Updates

---
+++ precache-manifest.e5eb7affe9ad20e494c46d29fddbeda9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48eac5d102a1b0e3481d4d20eafe6d4a",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "026e06494723516b7eca",
+    "url": "/monsters_rolodex/static/css/main.598112d6.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters_rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters_rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026e06494723516b7eca",
+    "url": "/monsters_rolodex/static/js/main.efdc43f8.chunk.js"
+  },
+  {
+    "revision": "8ad9bfbc5f4e24789e73",
+    "url": "/monsters_rolodex/static/js/runtime-main.8d3c5207.js"
+  }
+]);
